e2962e6b6c6460bf3da013219213e1d26512594b
alan-6565 <153258467+alan-6565@users.noreply.github.com>
2025-09-24 20:33:34 -0700

    updates on workbook

---
+++ Workbood1d/StringApplication/src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+
+        int numbers = 5;
+        String product = "pizza slices";
+        double price = 4.25;
+
+        System.out.println("your total for " + numbers + " " + product + " will be $" + price);
+    }
+}

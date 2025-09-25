5eca193f76cc952ab2c670e1d6c8d93c81861286
alan-6565 <153258467+alan-6565@users.noreply.github.com>
2025-09-24 20:21:12 -0700

    updates on workbook

---
+++ Workbood1d/StringApplication/src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String FavColor = "Red";
+        int YearClassStarted = 2025;
+        char middleNameInitial = 'A';
+        boolean havePets = true;
+        String message = "Have a great day!";
+
+        System.out.println("my favorite color is " + FavColor + " i started this class " + "in " + YearClassStarted + " my middle initial is  " + middleNameInitial);
+        System.out.println("do i have pets? : " + havePets);
+        System.out.println(message);
+//1. Declare a variable to store your favorite color
+//2. Declare a variable to store the year you started this class
+//3. Declare a variable to store your middle initial
+//4. Declare a variable to store whether you have pets (yes/no)
+//5. Declare a variable with a nice message or saying
+
+
+
+//1. Declare AND initialize variables for:
+//2. The number of days in a week
+//3. The price of coffee ($4.99)
+//4. Your favorite letter
+//5. Whether it's raining (true/false)
+
+    }
+}

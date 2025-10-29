a59d9f228f959ebc813d2227cf4df7993b8a76c7
alan-6565 <153258467+alan-6565@users.noreply.github.com>
2025-10-29 10:02:42 -0700

    workbook 5 completing

--- Workbook5/Vehicles/src/main/java/com/pluralsight/Moped.java
+++ Workbook5/Vehicles/src/main/java/com/pluralsight/Moped.java
@@ -1,3 +1,6 @@
 package com.pluralsight;
 class Moped extends Vehicle{
+    public Moped(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity) {
+        super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
+    }
 }
--- Workbook5/Vehicles/src/main/java/com/pluralsight/VehicleMain.java
+++ Workbook5/Vehicles/src/main/java/com/pluralsight/VehicleMain.java
@@ -2,10 +2,7 @@ package com.pluralsight;
 
 public class VehicleMain {
     public static void main(String[] args) {
-        Moped harley = new Moped();
-        harley.setColor("Blue");
-        harley.setFuelCapacity(6);
-        System.out.println(harley.getColor());
-        System.out.println(harley.getFuelCapacity());
+        Hovercraft hover = new Hovercraft("red", 4, 2, 12);
+        System.out.println(hover.getColor());
     }
 }
